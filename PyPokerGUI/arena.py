a119b6d45730a9c0f266d47427d58f8662ba5a59
zhengfuli <zhengfuli@bupt.edu.cn>
2018-04-17 19:19:08 +0800

    Migrated from an old repo

---
+++ PyPokerGUI/arena.py
@@ -0,0 +1,19 @@
+# PyPokerGUI for GUI, with reference from PyPokerEngine
+# https://github.com/ishikota/PyPokerGUI
+# https://github.com/ishikota/PyPokerEngine
+
+# To install:
+# pip install pypokergui
+
+# Default installation directory example:
+# /home/dell/anaconda2/lib/python2.7/site-packages
+
+# lupa for utilizing lua code of DeepStack
+
+# To install
+# apt-get install liblua5.2-dev
+# pip install lupa
+
+# PyPokerGUI requires tornado to be 4.4.2
+# You may need to modify this in METADATA and meta.json files in the pypokergui directory
+
---
+++ PyPokerGUI/game_customization.py
@@ -0,0 +1,12 @@
+class GameCustomization:
+    # pypokerengine/engine/round_manager/__deal_holecard
+    # ../engine/player/add_holecard
+    hole_card_count = 1
+
+    # pypokerengine/engine/round_manager/__deal_holecard
+    public_card_count = 1
+
+    # __turn and __river in the directory
+    has_turn = False
+    has_river = False
+
